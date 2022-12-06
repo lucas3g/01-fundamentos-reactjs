c23b5c90ad5280985c8f38a6f6759961b3060779
lucas3g <lucassilva080397@gmail.com>
2022-12-05 23:19:18 -0300

    feat: side bar and icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import { Header } from "./components/Header"
+import { Header } from "./components/Header";
 
-import './global.css'
+import styles from './App.module.css';
+
+import './global.css';
+import { Sidebar } from "./components/Sidebar";
+
 
 function App() {
   return (
     <div >
       <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+
+        </main>
+      </div>
     </div>
   )
 }
