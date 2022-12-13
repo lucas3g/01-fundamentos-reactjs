8aa7b968311bc3b48e9192172fd8a9adce866e72
lucas3g <lucassilva080397@gmail.com>
2022-12-13 14:52:12 -0300

    feat: add Comment

---
+++ src/components/Comment.jsx
@@ -0,0 +1,38 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment(){
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/lucas3g.png"/>
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Lucas Silva</strong>
+              <time title='06 de Dezembro às 22:22h' 
+                    dateTime="2022-12-06 22:22:34">
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+
+            <button title='Apagar comentário'>
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>Muito bom Devom, parabéns</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
